a1ce47cea36a8401f273be1cac067803c1d45233
grpm98 <gustavopuigbo@gmail.com>
2022-10-20 15:57:16 +0200

    Basic movement

---
+++ SAS Like Game/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+
+    [SerializeField] private float speed;
+
+    private Rigidbody rb;
+
+    float xInput;
+    float zInput;
+
+    private void Awake()
+    {
+        rb = GetComponent <Rigidbody> ();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyInput();
+    }
+
+  
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(xInput * speed * Time.fixedDeltaTime, 0, -zInput * speed * Time.fixedDeltaTime),ForceMode.Force);
+    }
+
+    private void MyInput()
+    {
+        zInput = Input.GetAxisRaw("Horizontal");
+        xInput = Input.GetAxisRaw("Vertical");
+    }
+}
